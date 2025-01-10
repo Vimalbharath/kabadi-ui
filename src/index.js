0ed258fe6c43641d60b8939be24228be92a00e24
Vimalbharath <vimalbharath21@gmail.com>
2025-01-11 01:22:39 +0530

    new react 18 feature

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDom from 'react-dom'
+import ReactDom from 'react-dom/client';
+//import { createRoot } from 'react-dom/client';
+// const container = document.getElementById('root');
+// const root = ReactDom.createRoot(container); // createRoot(container!) if you use TypeScript
+// root.render(<App />);
 
 
 function App(){
   console.log('vimal');
- return (<h4 >Hello Vimal</h4>); 
+ return (<h4>Hello Vimal</h4>); 
 }
 
-ReactDom.render(<App/>, document.getElementById('root'));
+ReactDom.createRoot( document.getElementById('root')).render(<App/>);

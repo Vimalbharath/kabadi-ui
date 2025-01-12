3cdcc720e7e2ca549b460f68543021841b1f535f
Vimalbharath <vimalbharath21@gmail.com>
2025-01-12 19:19:44 +0530

    added api using axios

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { useState,useEffect } from 'react';
+import { api } from '../misc/api'
 import Player from './Player';
 // const url = 'http://localhost:8080/players'
 // const [loading,setLoading]=useState(true);
-// const [tours,setTours]=useState([]);
+
 //   // const removeTour=(id)=>{
 //   //   const newTours=tours.filter((tour)=>tour.id!==id);
 //   //   setTours(newTours);
@@ -32,18 +34,36 @@ import Player from './Player';
 //  return (<h4>Hello Vimalbharath</h4>); 
 // }
 const Players = () => {
+
+  const [tours,setTours]=useState([]);
+
+  const handlegetallplayers = async () => {
+    try {
+      // setIsBooksLoading(true)
+      const response = await api.getallplayers();
+      console.log(response.data);
+       setTours(response.data)
+    } catch (error) {
+      // handleLogError(error)
+    } finally {
+      // setIsBooksLoading(false)
+    }
+  }
+  useEffect(() => {
+    handlegetallplayers()
+  }, [])
   return (
   <section>
     <div >
       <h2>our Players</h2>
       
     </div>
-    {/* <div>
-      {tours.map((tour)=>{
-      return <Player key={tour.playerid}{...tour}></Player>
-      }
-      )}
-    </div> */}
+      <div>
+        {tours.map((tour)=>{
+        return <Player key={tour.playerid}{...tour}></Player>
+        }
+        )}
+      </div>
   </section>
   );
 };

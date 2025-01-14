be3ce50f1337fae2433dcc12eafe3588a58bc940
Vimalbharath <vimalbharath21@gmail.com>
2025-01-14 19:16:48 +0530

    improvised navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../Navbar.css'; // Import the CSS file
 
 function Navbar() {
-
   return (
-    
-    <nav>
-        
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">
+            Home
+          </Link>
         </li>
-        <li>
-          <Link to='/login'>Login</Link>
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Login
+          </Link>
         </li>
-        <li>
-          <Link to='/signup'>Signup</Link>
+        <li className="nav-item">
+          <Link to="/signup" className="nav-link">
+            Signup
+          </Link>
         </li>
-        <li>
-          <Link to='/teams'>Teams</Link>
+        <li className="nav-item">
+          <Link to="/teams" className="nav-link">
+            Teams
+          </Link>
         </li>
-        <li>
-          <Link to='/players'>Players</Link>
+        <li className="nav-item">
+          <Link to="/players" className="nav-link">
+            Players
+          </Link>
         </li>
-        <li>
-          <Link to='/matches'>Matches</Link>
+        <li className="nav-item">
+          <Link to="/matches" className="nav-link">
+            Matches
+          </Link>
         </li>
       </ul>
     </nav>
   );
 }
 
-export default Navbar
+export default Navbar;
